e3acaa7226d76d4dd623f1cec6abf618dfe20f17
darkila26 <darkila26@gmail.com>
2022-05-21 18:56:32 -0300

    Adds script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Carrega o javascript");
+
+// Function to validate CPF which is tax identification number in Brazil
+
+function validacao () {
+    console.log("Iniciando a validação de CPF");
+}
