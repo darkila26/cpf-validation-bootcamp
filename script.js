39bb2d624b6e65e0fde2a302efa72b632db59fee
darkila26 <darkila26@gmail.com>
2022-05-21 19:47:21 -0300

    Gets the input form value and shows a message for validaCPF function as true or false

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-console.log("Carrega o javascript");
+console.log('Carrega o javascript');
+
+// Checks if the CPF is correct
+
+function validaCPF() {
+    return false;
+}
 
 // Function to validate CPF which is tax identification number in Brazil
 
-function validacao () {
-    console.log("Iniciando a validação de CPF");
+function validacao() {
+    console.log('Iniciando a validação de CPF');
+
+    // Gets CPF number, value from input form
+
+    var cpf = document.getElementById('cpf_digitado').value;
+
+    var resultadoValidacao = validaCPF(cpf);
+
+    if (resultadoValidacao) {
+        document.getElementById('success').style.display = 'block';
+    }
+    else {
+        document.getElementById('error').style.display = 'block';
+
+    }
 }
+
+
+
